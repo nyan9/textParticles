2f0a6f710edd38e000c120cc068503133c1770d3
nyan9 <rynnaing@gmail.com>
2021-04-07 12:18:58 -0400

    move one circle on canvas

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,5 +1,19 @@
+const width = window.innerWidth;
+const height = window.innerHeight;
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-canvas.height = window.innerWidth;
+canvas.width = width;
+canvas.height = height;
+
+import Node from "./node";
+
+let node;
+function setupNode() {
+  node = new Node(width / 2, height / 2);
+}
+setupNode();
+
+function showNode() {
+  node.animate(width, height);
+}
+showNode();
---
+++ src/scripts/node.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+class Node {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 50;
+    this.initialX = this.x;
+    this.initialY = this.y;
+    this.velX = 0.1;
+    this.velY = 0.1;
+    this.dt = 10;
+
+    this.move = this.move.bind(this);
+    this.draw = this.draw.bind(this);
+    this.animate = this.animate.bind(this);
+    this.clearNode = this.clearNode.bind(this);
+  }
+
+  move() {
+    // position = initial position + velocity * deltaTime
+    this.x = this.x + this.velX * this.dt;
+    this.y = this.y + this.velY * this.dt;
+  }
+
+  draw() {
+    ctx.fillStyle = "rgb(255,56,100)";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  clearNode() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  animate() {
+    this.clearNode();
+    setTimeout(this.animate, this.dt);
+    this.move();
+    this.draw();
+  }
+}
+
+export default Node;
